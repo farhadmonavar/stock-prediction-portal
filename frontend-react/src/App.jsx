3887d42466892c90a68ba1276c9f4890ce18253d
farhadmonavar <farhad.monavar@gmail.com>
2026-07-28 11:54:49 +0330

    feat: initialize DRF backend and React frontend applications

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
